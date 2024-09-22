bd19135bca8ee6366ff49b29b8c9afc24e26b2e8
Juliette Chevallier <juliette.chevallier@insa-toulouse.fr>
2024-09-22 19:38:45 +0200

    init 2425

--- RNN/solutions/imdb/rnn.py
+++
@@ -1,9 +0,0 @@
-embedding_size = 32
-
-rnn = Sequential(name="RNN")
-rnn.add(layers.Embedding(vocab_size, embedding_size, input_length=max_words))
-rnn.add(layers.LSTM(int(.5*embedding_size)))
-rnn.add(layers.Dropout(0.1))
-rnn.add(layers.Dense(1, activation='sigmoid'))
-
-print(rnn.summary())
